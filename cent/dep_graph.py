53c85ed65f0b3f2a485d85e27f0e8b456e667aaa
Wapiti08 <tzrzhuoran@gmail.com>
2024-11-21 11:11:20 +0000

    update dependency graph to optimize it

--- cent/dep_graph.py
+++ cent/dep_graph.py
@@ -12,7 +12,8 @@ import pickle
 import networkx as nx
 from functools import partial
 from tqdm import tqdm
-
+import os
+import uuid
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s [%(levelname)s]: %(message)s',
@@ -39,13 +40,14 @@ def process_edges_chunk(chunk, release_nodes, time_ranges, nodes, rel_to_soft,
     Worker function to process a chunk of edges.
     """
     local_graph = nx.DiGraph()
-    for src, tgt in chunk:
+    rel_to_soft_map = rel_to_soft()
+    for src, tgt, _ in chunk:
         if is_release(nodes[src]):  # Only consider release nodes for the source
             src_range = time_ranges.get(src, (0, float('inf')))
             if not is_release(nodes[tgt]):  # If the target is a software node
                 # Get all releases for the target software
-                if tgt in rel_to_soft():  # Check if the software has associated releases
-                    for release in rel_to_soft()[tgt]:  # Iterate through all releases of the software
+                if tgt in rel_to_soft_map:  # Check if the software has associated releases
+                    for release in rel_to_soft_map[tgt]:  # Iterate through all releases of the software
                         if release in release_nodes:  # Ensure the release exists in the graph
                             tgt_timestamp = get_timestamp(nodes[release])  # Timestamp of the target release
                             if src_range[0] <= tgt_timestamp < src_range[1]:  # Check if within range
@@ -55,18 +57,23 @@ def process_edges_chunk(chunk, release_nodes, time_ranges, nodes, rel_to_soft,
                 tgt_timestamp = get_timestamp(nodes[tgt])  # Timestamp of the target release
                 if src_range[0] <= tgt_timestamp < src_range[1]:
                     local_graph.add_edge(src, tgt)  # Add direct edge between releases
-    return local_graph
+    
+    # save the subgraph to disk
+    subgraph_file = Path.cwd().parent.joinpath('data', f"subgraph_{os.getpid()}_{uuid.uuid4().hex}.graphml")
+    nx.write_graphml(local_graph, subgraph_file)
 
-
-
+    return subgraph_file
 
 class DepGraph:
     def __init__(self, nodes, edges):
         self.nodes = nodes
         self.edges = edges
     
-
-
+    def cve_check(self, node:dict):
+        if 'type' in node and node['type'] == "CVE" and self.str_to_json(node["value"])['cve'] !=[]:
+            return True
+        else:
+            return False
 
     def get_timestamp(self, node: dict):
         return int(node.get("timestamp", 0))
@@ -88,6 +95,9 @@ class DepGraph:
     def get_releases(self,):
         return {node_id: data for node_id, data in self.nodes.items() if data['labels'] == ":Release"}
     
+    def get_cve_releases(self,):
+        return {node_id: data for node_id, data in self.nodes.items() if data['labels'] == ":Release" and self.cve_check(data)}
+
     def rel_to_soft(self):
         ''' build the dict to map parent software to release
         
@@ -123,34 +133,37 @@ class DepGraph:
     def time_ranges(self, software_to_release: dict):
         timestamp_ranges = {}
         for software, releases in software_to_release.items():
+            timestamps = [ts for _, ts in releases] + [float('inf')]  # Add open-ended range
             for i, (nid, timestamp) in enumerate(releases):
-                if i < len(releases) - 1:
-                    next_timestamp = releases[i + 1][1]
-                else:
-                    next_timestamp = float('inf')  # Open-ended range for the last release
-                timestamp_ranges[nid] = (timestamp, next_timestamp)
+                timestamp_ranges[nid] = (timestamp, timestamps[i + 1])
         return timestamp_ranges
 
     def filter_edges(self, software_to_releases):
         filter_edges = []
         for src, tgt, attr in self.edges:
-            if attr['label'] == 'dependency':
-                # release to software
-                filter_edges.append((src, tgt))
-            elif attr['label'] == 'relationship_AR':
-                # release to software
-                filter_edges.append((tgt, src))
-            else:
-                continue
-        return filter_edges
-
+            if attr['label'] in {'dependency', 'relationship_AR'}:
+                yield (src, tgt if attr['label'] == 'dependency' else tgt, src)
+    
+    def chunk_generator(self, generator, chunk_size):
+        """
+        Breaks a generator into chunks of size `chunk_size`.
+        """
+        chunk = []
+        for item in generator:
+            chunk.append(item)
+            if len(chunk) == chunk_size:
+                yield chunk
+                chunk = []
+        if chunk:  # Yield remaining items
+            yield chunk
 
     def dep_graph_build_parallel(self, filter_edges, time_ranges):
         """
         Parallelized version of dep_graph_build.
         """
         # Get release nodes
-        release_nodes = self.get_releases()
+        # release_nodes = self.get_releases()
+        release_nodes = self.get_cve_releases()
         # Precompute other reusable data
         nodes = self.nodes
         rel_to_soft = self.rel_to_soft
@@ -159,12 +172,12 @@ class DepGraph:
         
         # Split edges into chunks for parallel processing
         num_processes = cpu_count()
-        chunk_size = (len(filter_edges) + num_processes - 1) // num_processes  # Ceiling division
-        edge_chunks = [filter_edges[i:i + chunk_size] for i in range(0, len(filter_edges), chunk_size)]
+        chunk_size = 200000
+        edge_chunks = self.chunk_generator(filter_edges, chunk_size)
         
         # Use multiprocessing pool to process chunks in parallel
         with Pool(processes=num_processes) as pool:
-            results = list(tqdm(
+            subgraph_files = list(tqdm(
                 pool.imap(
                     partial(process_wrapper,  # Pass the global function
                             release_nodes=release_nodes, time_ranges=time_ranges, nodes=nodes,
@@ -172,18 +185,16 @@ class DepGraph:
                     edge_chunks
                 ),
                 desc="Parallel graph build",
-                total=len(edge_chunks)
             ))
 
         # Combine all subgraphs into one graph
-        new_graph = nx.DiGraph()
-        for nid, data in release_nodes.items():
-            new_graph.add_node(nid, **data)
+        combined_graph = nx.DiGraph()
+        for subgraph_file in subgraph_files:
+            subgraph = nx.read_graphml(subgraph_file)
+            combined_graph.update(subgraph)
+            os.remove(subgraph_file)
         
-        for subgraph in results:
-            new_graph.update(subgraph)
-        
-        return new_graph
+        return combined_graph
 
     # def dep_graph_build(self, filter_edges, time_ranges):
     #     new_graph = nx.DiGraph()
@@ -228,67 +239,67 @@ def load_data(file_path):
 
 if __name__ == "__main__":
 
-    nodes_edges_path = Path.cwd().parent.joinpath("data", 'graph_nodes_edges.pkl')
-    nodes, edges = load_data(nodes_edges_path)
+    # nodes_edges_path = Path.cwd().parent.joinpath("data", 'graph_nodes_edges.pkl')
+    # nodes, edges = load_data(nodes_edges_path)
 
-    # nodes = {
-    # "n1":  {'labels': ':AddedValue', 
-    #         'id': 'org.wso2.carbon.apimgt:forum:6.5.275:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'
-    #         },
-    # "n2": {'labels': ':AddedValue', 
-    #        'id': 'org.wso2.carbon.apimgt:forum:6.5.276:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'
-    #        },
-    # "n3": {'labels': ':AddedValue', 'id': 'org.wso2.carbon.apimgt:forum:6.5.272:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'
-    #        },
-    # "n4": {'labels': ':AddedValue', 'id': 'org.wso2.carbon.apimgt:forum:6.5.279:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'},
-    # "n5": {'labels': ':AddedValue', 'id': 'org.wso2.carbon.apimgt:forum:6.5.278:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'},
-    # "n6": {'labels': ':AddedValue', 'value': '1', 'id': 'io.gravitee.common:gravitee-common:3.1.0:POPULARITY_1_YEAR', 'type': 'POPULARITY_1_YEAR'},
-    # "n7": {'labels': ':AddedValue', 'value': '2', 'id': 'org.thepalaceproject.audiobook:org.librarysimplified.audiobook.parser.api:11.0.0:POPULARITY_1_YEAR', 'type': 'POPULARITY_1_YEAR'},
-    # "n8": {'labels': ':AddedValue', 'value': '1', 'id': 'com.emergetools.snapshots:snapshots-shared:0.8.1:POPULARITY_1_YEAR', 'type': 'POPULARITY_1_YEAR'},
-    # "n9": {'labels': ':AddedValue', 'id': 'se.fortnox.reactivewizard:reactivewizard-jaxrs:SPEED', 'type': 'SPEED', 'value': '0.08070175438596491'},
-    # "n10":{'labels': ':AddedValue', 'id': 'cc.akkaha:asura-dubbo_2.12:SPEED', 'type': 'SPEED', 'value': '0.029411764705882353'},
-    # "n11":{'labels': ':AddedValue', 'id': 'it.tidalwave.thesefoolishthings:it-tidalwave-thesefoolishthings-examples-dci-swing:SPEED', 'type': 'SPEED', 'value': '0.014814814814814815'},
-    # "n12":{'labels': ':AddedValue', 'id': 'com.softwaremill.sttp.client:core_sjs0.6_2.13:2.0.2:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"7\\",\\"outdatedTimeInMs\\":\\"3795765000\\"}}'},
-    # "n13":{'labels': ':AddedValue', 'id': 'com.ibeetl:act-sample:3.0.0-M6:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"2\\",\\"outdatedTimeInMs\\":\\"11941344000\\"}}'},
-    # "n14":{'labels': ':AddedValue', 'id': 'com.softwaremill.sttp.client:core_sjs0.6_2.13:2.0.0:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"9\\",\\"outdatedTimeInMs\\":\\"4685281000\\"}}'},
-    # "n15":{'labels': ':AddedValue', 'id': 'com.lihaoyi:ammonite_2.12.1:0.9.8:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"367\\",\\"outdatedTimeInMs\\":\\"142773884000\\"}}'},
-    # "n0":{'labels': ':AddedValue', 'id': 'com.yahoo.vespa:container-disc:7.394.21:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"448\\",\\"outdatedTimeInMs\\":\\"105191360000\\"}}'},
-    # 'n16': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.111', 'version': '5.20.111', 'timestamp': '1626148242000'},
-    # 'n17': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M4', 'version': '1.0.0-M4', 'timestamp': '1583239943000'},
-    # 'n18': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M3', 'version': '1.0.0-M3', 'timestamp': '1579861029000'},
-    # 'n19': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.113', 'version': '5.20.113', 'timestamp': '1626179580000'},
-    # 'n20': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.112', 'version': '5.20.112', 'timestamp': '1626170945000'},
-    # 'n21': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.115', 'version': '5.20.115', 'timestamp': '1626340086000'},
-    # 'n22': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M2', 'version': '1.0.0-M2', 'timestamp': '1576600059000'},
-    # 'n23': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M6', 'version': '1.0.0-M6', 'timestamp': '1586476381000'},
-    # 'n24': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.114', 'version': '5.20.114', 'timestamp': '1626266264000'},
-    # 'n25': {'labels': ':Release', 'version': '0.5.0', 'timestamp': '1669329622000', 'id': 'com.splendo.kaluga:alerts-androidlib:0.5.0'},
+    nodes = {
+    "n1":  {'labels': ':AddedValue', 
+            'id': 'org.wso2.carbon.apimgt:forum:6.5.275:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'
+            },
+    "n2": {'labels': ':AddedValue', 
+           'id': 'org.wso2.carbon.apimgt:forum:6.5.276:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'
+           },
+    "n3": {'labels': ':AddedValue', 'id': 'org.wso2.carbon.apimgt:forum:6.5.272:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'
+           },
+    "n4": {'labels': ':AddedValue', 'id': 'org.wso2.carbon.apimgt:forum:6.5.279:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'},
+    "n5": {'labels': ':AddedValue', 'id': 'org.wso2.carbon.apimgt:forum:6.5.278:CVE', 'type': 'CVE', 'value': '{\\"cve\\":[{\\"cwe\\":\\"[CWE-20]\\",\\"severity\\":\\"MODERATE\\",\\"name\\":\\"CVE-2023-6835\\"}]}'},
+    "n6": {'labels': ':AddedValue', 'value': '1', 'id': 'io.gravitee.common:gravitee-common:3.1.0:POPULARITY_1_YEAR', 'type': 'POPULARITY_1_YEAR'},
+    "n7": {'labels': ':AddedValue', 'value': '2', 'id': 'org.thepalaceproject.audiobook:org.librarysimplified.audiobook.parser.api:11.0.0:POPULARITY_1_YEAR', 'type': 'POPULARITY_1_YEAR'},
+    "n8": {'labels': ':AddedValue', 'value': '1', 'id': 'com.emergetools.snapshots:snapshots-shared:0.8.1:POPULARITY_1_YEAR', 'type': 'POPULARITY_1_YEAR'},
+    "n9": {'labels': ':AddedValue', 'id': 'se.fortnox.reactivewizard:reactivewizard-jaxrs:SPEED', 'type': 'SPEED', 'value': '0.08070175438596491'},
+    "n10":{'labels': ':AddedValue', 'id': 'cc.akkaha:asura-dubbo_2.12:SPEED', 'type': 'SPEED', 'value': '0.029411764705882353'},
+    "n11":{'labels': ':AddedValue', 'id': 'it.tidalwave.thesefoolishthings:it-tidalwave-thesefoolishthings-examples-dci-swing:SPEED', 'type': 'SPEED', 'value': '0.014814814814814815'},
+    "n12":{'labels': ':AddedValue', 'id': 'com.softwaremill.sttp.client:core_sjs0.6_2.13:2.0.2:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"7\\",\\"outdatedTimeInMs\\":\\"3795765000\\"}}'},
+    "n13":{'labels': ':AddedValue', 'id': 'com.ibeetl:act-sample:3.0.0-M6:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"2\\",\\"outdatedTimeInMs\\":\\"11941344000\\"}}'},
+    "n14":{'labels': ':AddedValue', 'id': 'com.softwaremill.sttp.client:core_sjs0.6_2.13:2.0.0:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"9\\",\\"outdatedTimeInMs\\":\\"4685281000\\"}}'},
+    "n15":{'labels': ':AddedValue', 'id': 'com.lihaoyi:ammonite_2.12.1:0.9.8:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"367\\",\\"outdatedTimeInMs\\":\\"142773884000\\"}}'},
+    "n0":{'labels': ':AddedValue', 'id': 'com.yahoo.vespa:container-disc:7.394.21:FRESHNESS', 'type': 'FRESHNESS', 'value': '{\\"freshness\\":{\\"numberMissedRelease\\":\\"448\\",\\"outdatedTimeInMs\\":\\"105191360000\\"}}'},
+    'n16': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.111', 'version': '5.20.111', 'timestamp': '1626148242000'},
+    'n17': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M4', 'version': '1.0.0-M4', 'timestamp': '1583239943000'},
+    'n18': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M3', 'version': '1.0.0-M3', 'timestamp': '1579861029000'},
+    'n19': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.113', 'version': '5.20.113', 'timestamp': '1626179580000'},
+    'n20': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.112', 'version': '5.20.112', 'timestamp': '1626170945000'},
+    'n21': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.115', 'version': '5.20.115', 'timestamp': '1626340086000'},
+    'n22': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M2', 'version': '1.0.0-M2', 'timestamp': '1576600059000'},
+    'n23': {'labels': ':Release', 'id': 'org.apache.camel.quarkus:camel-quarkus-kotlin-parent:1.0.0-M6', 'version': '1.0.0-M6', 'timestamp': '1586476381000'},
+    'n24': {'labels': ':Release', 'id': 'org.wso2.carbon.identity.framework:org.wso2.carbon.identity.cors.mgt.core:5.20.114', 'version': '5.20.114', 'timestamp': '1626266264000'},
+    'n25': {'labels': ':Release', 'version': '0.5.0', 'timestamp': '1669329622000', 'id': 'com.splendo.kaluga:alerts-androidlib:0.5.0'},
 
-    # }
+    }
 
 
-    # # Example edges
-    # edges = [
-    #     ("n1", "n2", {"label": "relationship_AR"}),
-    #     ("n1", "n12", {"label": "relationship_AR"}),
-    #     ("n5", "n3", {"label": "relationship_AR"}),
-    #     ("n1", "n6", {"label": "relationship_AR"}),
-    #     ("n7", "n3", {"label": "relationship_AR"}),
-    #     ("n1", "n11", {"label": "relationship_AR"}),
-    #     ("n8", "n3", {"label": "relationship_AR"}),
-    #     ("n4", "n7", {"label": "dependency"}),
-    #     ("n10", "n12", {"label": "relationship_AR"}),
-    #     ("n5", "n13", {"label": "relationship_AR"}),
-    #     ("n4", "n14", {"label": "relationship_AR"}),
-    #     ("n13", "n0", {"label": "dependency"}),
-    #     ("n10", "n15", {"label": "relationship_AR"}),
-    #     ("n1", "n16", {"label": "dependency"}),
-    #     ("n19", "n25", {"label": "relationship_AR"}),
-    #     ("n5", "n21", {"label": "relationship_AR"}),
-    #     ("n21", "n23", {"label": "relationship_AR"}),
-    #     ("n19", "n24", {"label": "relationship_AR"}),
-    #     ("n4", "n19", {"label": "relationship_AR"}),
-    # ]
+    # Example edges
+    edges = [
+        ("n1", "n2", {"label": "relationship_AR"}),
+        ("n1", "n12", {"label": "relationship_AR"}),
+        ("n5", "n3", {"label": "relationship_AR"}),
+        ("n1", "n6", {"label": "relationship_AR"}),
+        ("n7", "n3", {"label": "relationship_AR"}),
+        ("n1", "n11", {"label": "relationship_AR"}),
+        ("n8", "n3", {"label": "relationship_AR"}),
+        ("n4", "n7", {"label": "dependency"}),
+        ("n10", "n12", {"label": "relationship_AR"}),
+        ("n5", "n13", {"label": "relationship_AR"}),
+        ("n4", "n14", {"label": "relationship_AR"}),
+        ("n13", "n0", {"label": "dependency"}),
+        ("n10", "n15", {"label": "relationship_AR"}),
+        ("n1", "n16", {"label": "dependency"}),
+        ("n19", "n25", {"label": "relationship_AR"}),
+        ("n5", "n21", {"label": "relationship_AR"}),
+        ("n21", "n23", {"label": "relationship_AR"}),
+        ("n19", "n24", {"label": "relationship_AR"}),
+        ("n4", "n19", {"label": "relationship_AR"}),
+    ]
     
 
     dep_graph_path = Path.cwd().parent.joinpath("data", "dep_graph.pkl")
